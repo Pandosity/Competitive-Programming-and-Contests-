36d0940e9cbfa357f338b790b03f60cb5a31b6fe
Pandosity <dario.rancati@sns.it>
2019-01-17 00:15:15 +0100

    Update find_pair.cpp

--- Lesson_6/find_pair.cpp
+++ Lesson_6/find_pair.cpp
@@ -1,3 +1,6 @@
+/*This algorithm implements the basic idea to sort numbers, and then subtract to k the cardinality of sets of equal pairs, sorted in 
+lexicographical order (first row by row, then cell by cell). Space and time are both linear.*/
+
 #include<bits/stdc++.h>
 #define dbg(x) cout<<#x<<": "<<x<<endl;
 using namespace std;
@@ -21,9 +24,6 @@ int main(){
 	mult.reserve(n);
 	
 	sort(numbers.begin(), numbers.end(), sorter);
-	//for(int i = 0; i < n; ++i){
-	//	dbg(numbers[i]);
-	//}
 	
 	ll count = 1;
 	
@@ -35,29 +35,19 @@ int main(){
 		else ++count;
 	}
 	ll t = mult.size();
-	//for(int i = 0; i < t; ++i){
-	//	dbg(mult[i].first);
-	//	dbg(mult[i].second);
-	//}
 	
 	ll a = 0;
 	ll b = 0;
-	
-	//for(ll i = 1; i < t; ++i){
-	//	mult[i].second += mult[i-1].second;
-	//}
 	
 	while(k > mult[a].second*n){
 		k -= mult[a].second*n;
 		++a;
 	}
-	//dbg(a);
 	
 	while(k > mult[b].second*mult[a].second){
 		k -= mult[b].second*mult[a].second;
 		++b;
 	}
-	//dbg(b);
 	
 	cout << mult[a].first << " " << mult[b].first;
 	return 0;
